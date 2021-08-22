f1db564c26866c3d16f91eedd5b8ef7c2375e0e0
Jason Umiker <jason.umiker@gmail.com>
2021-08-22 11:19:45 +1000

    Updated Calico and SSM Agent

--- cluster-bootstrap/eks_cluster.py
+++ cluster-bootstrap/eks_cluster.py
@@ -857,386 +857,42 @@ class EKSClusterStack(core.Stack):
             # and https://github.com/aws/amazon-vpc-cni-k8s/tree/master/charts/aws-calico
 
             # First we need to install the CRDs which are not part of the Chart
-            calico_crds_manifest_1 = eks_cluster.add_manifest("CalicoCRDManifest1",            
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "felixconfigurations.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "FelixConfiguration",
-                    "plural": "felixconfigurations",
-                    "singular": "felixconfiguration"
-                    }
-                }
-                })
-            calico_crds_manifest_2 = eks_cluster.add_manifest("CalicoCRDManifest2",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "ipamblocks.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "IPAMBlock",
-                    "plural": "ipamblocks",
-                    "singular": "ipamblock"
-                    }
-                }
-                })
-            calico_crds_manifest_3 = eks_cluster.add_manifest("CalicoCRDManifest3",            
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "blockaffinities.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "BlockAffinity",
-                    "plural": "blockaffinities",
-                    "singular": "blockaffinity"
-                    }
-                }
-                })
-            calico_crds_manifest_4 = eks_cluster.add_manifest("CalicoCRDManifest4",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "bgpconfigurations.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "BGPConfiguration",
-                    "plural": "bgpconfigurations",
-                    "singular": "bgpconfiguration"
-                    }
-                }
-                })
-            calico_crds_manifest_5 = eks_cluster.add_manifest("CalicoCRDManifest5",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "bgppeers.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "BGPPeer",
-                    "plural": "bgppeers",
-                    "singular": "bgppeer"
-                    }
-                }
-                })
-            calico_crds_manifest_6 = eks_cluster.add_manifest("CalicoCRDManifest6",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "ippools.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "IPPool",
-                    "plural": "ippools",
-                    "singular": "ippool"
-                    }
-                }
-                })
-            calico_crds_manifest_7 = eks_cluster.add_manifest("CalicoCRDManifest7",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "hostendpoints.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "HostEndpoint",
-                    "plural": "hostendpoints",
-                    "singular": "hostendpoint"
-                    }
-                }
-                })
-            calico_crds_manifest_8 = eks_cluster.add_manifest("CalicoCRDManifest8",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "clusterinformations.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "ClusterInformation",
-                    "plural": "clusterinformations",
-                    "singular": "clusterinformation"
-                    }
-                }
-                })
-            calico_crds_manifest_9 = eks_cluster.add_manifest("CalicoCRDManifest9",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "globalnetworkpolicies.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "GlobalNetworkPolicy",
-                    "plural": "globalnetworkpolicies",
-                    "singular": "globalnetworkpolicy"
-                    }
-                }
-                })
-            calico_crds_manifest_10 = eks_cluster.add_manifest("CalicoCRDManifest10",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "globalnetworksets.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Cluster",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "GlobalNetworkSet",
-                    "plural": "globalnetworksets",
-                    "singular": "globalnetworkset"
-                    }
-                }
-                })
-            calico_crds_manifest_11 = eks_cluster.add_manifest("CalicoCRDManifest11",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "networkpolicies.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Namespaced",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "NetworkPolicy",
-                    "plural": "networkpolicies",
-                    "singular": "networkpolicy"
-                    }
-                }
-                })
-            calico_crds_manifest_12 = eks_cluster.add_manifest("CalicoCRDManifest12",
-                {
-                "apiVersion": "apiextensions.k8s.io/v1beta1",
-                "kind": "CustomResourceDefinition",
-                "metadata": {
-                    "name": "networksets.crd.projectcalico.org"
-                },
-                "spec": {
-                    "scope": "Namespaced",
-                    "group": "crd.projectcalico.org",
-                    "versions": [
-                    {
-                        "name": "v1",
-                        "served": True,
-                        "storage": True
-                    }
-                    ],
-                    "names": {
-                    "kind": "NetworkSet",
-                    "plural": "networksets",
-                    "singular": "networkset"
-                    }
-                }
-                })
+            # Import calico-crds.yaml to a list of dictionaries and submit them as a manifest to EKS
+            # Read the YAML file
+            calico_crds_yaml_file = open("calico-crds.yaml", 'r')
+            calico_crds_yaml = list(yaml.load_all(calico_crds_yaml_file, Loader=yaml.FullLoader))
+            calico_crds_yaml_file.close()
+            loop_iteration = 0
+            for value in calico_crds_yaml:
+                #print(value)
+                loop_iteration = loop_iteration + 1
+                manifest_id  = "CalicoCRD" + str(loop_iteration)
+                calico_crd = eks_cluster.add_manifest(manifest_id,value)
             # Then we can install the Helm Chart
             calico_np_chart = eks_cluster.add_helm_chart(
                 "calico",
                 chart="aws-calico",
-                version="0.3.4",
+                version="0.3.7",
                 release="calico",
                 repository="https://aws.github.io/eks-charts",
                 namespace="kube-system"
             )
-            # The Helm Chart depends on all the CRDs
-            calico_np_chart.node.add_dependency(calico_crds_manifest_1)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_2)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_3)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_4)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_5)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_6)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_7)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_8)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_9)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_10)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_11)
-            calico_np_chart.node.add_dependency(calico_crds_manifest_12)
+            calico_np_chart.node.add_dependency(calico_crd)
 
         # Deploy SSM Agent
         if (self.node.try_get_context("deploy_ssm_agent") == "True"):
-            # For more information see https://docs.aws.amazon.com/prescriptive-guidance/latest/patterns/install-ssm-agent-on-amazon-eks-worker-nodes-by-using-kubernetes-daemonset.html
-            ssm_agent_manifest = eks_cluster.add_manifest("SSMAgentManifest",
-            {
-                "apiVersion":"apps/v1",
-                "kind":"DaemonSet",
-                "metadata":{
-                    "labels":{
-                        "k8s-app":"ssm-installer"
-                    },
-                    "name":"ssm-installer",
-                    "namespace":"kube-system"
-                },
-                "spec":{
-                    "selector":{
-                        "matchLabels":{
-                            "k8s-app":"ssm-installer"
-                        }
-                    },
-                    "template":{
-                        "metadata":{
-                            "labels":{
-                            "k8s-app":"ssm-installer"
-                            }
-                        },
-                        "spec":{
-                            "containers":[
-                            {
-                                "image":"amazonlinux",
-                                "imagePullPolicy":"Always",
-                                "name":"ssm",
-                                "command":[
-                                    "/bin/bash"
-                                ],
-                                "args":[
-                                    "-c",
-                                    "echo '* * * * * root yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm & rm -rf /etc/cron.d/ssmstart' > /etc/cron.d/ssmstart"
-                                ],
-                                "securityContext":{
-                                    "allowPrivilegeEscalation":True
-                                },
-                                "volumeMounts":[
-                                    {
-                                        "mountPath":"/etc/cron.d",
-                                        "name":"cronfile"
-                                    }
-                                ],
-                                "terminationMessagePath":"/dev/termination-log",
-                                "terminationMessagePolicy":"File"
-                            }
-                            ],
-                            "volumes":[
-                            {
-                                "name":"cronfile",
-                                "hostPath":{
-                                    "path":"/etc/cron.d",
-                                    "type":"Directory"
-                                }
-                            }
-                            ],
-                            "dnsPolicy":"ClusterFirst",
-                            "restartPolicy":"Always",
-                            "schedulerName":"default-scheduler",
-                            "terminationGracePeriodSeconds":30
-                        }
-                    }
-                }
-            })
+            # For more information see https://github.com/aws-samples/ssm-agent-daemonset-installer
+            # Import ssm-agent.yaml to a list of dictionaries and submit them as a manifest to EKS
+            # Read the YAML file
+            ssm_agent_yaml_file = open("ssm-agent.yaml", 'r')
+            ssm_agent_yaml = list(yaml.load_all(ssm_agent_yaml_file, Loader=yaml.FullLoader))
+            ssm_agent_yaml_file.close()
+            loop_iteration = 0
+            for value in ssm_agent_yaml:
+                #print(value)
+                loop_iteration = loop_iteration + 1
+                manifest_id  = "SSMAgent" + str(loop_iteration)
+                eks_cluster.add_manifest(manifest_id,value)
 
         # If you have a 'True' in the deploy_bastion variable at the top of the file we'll deploy
         # a basion server that you can connect to via Systems Manager Session Manager
@@ -1298,7 +954,7 @@ class EKSClusterStack(core.Stack):
 
             # Wait to deploy Bastion until cluster is up and we're deploying manifests/charts to it
             # This could be any of the charts/manifests I just picked this one at random
-            bastion_instance.node.add_dependency(ssm_agent_manifest)
+            bastion_instance.node.add_dependency(metricsserver_chart)
 
 
         if (self.node.try_get_context("deploy_client_vpn") == "True"):
